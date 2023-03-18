1c7519bfc6b8518c2d099b29c3d4d6a24e45fd32
igor leonardo mendonça <igormendonca@rebortec.com>
2023-03-18 16:15:37 -0300

    initial page

--- www/src/app/modules/home/home.module.ts
+++ www/src/app/modules/home/home.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
+import { InitialPageComponent } from './pages/initial-page/initial-page.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    InitialPageComponent
+  ],
   imports: [
     CommonModule,
     HomeRoutingModule
